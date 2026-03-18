3d88aedcc8f36b7657b8b8ae2d134d26f9c483fd
isadahmerb <idahmerbarbosa@gmail.com>
2026-03-18 15:36:36 -0300

    add sozinha

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -3,11 +3,18 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         conta c1 = new conta("cc", "isa");
+        c1.Depositar();
+        c1.PagarT();
         c1.status();
+        c1.Sacar();
 
-
-
-
+        conta c2 = new conta("cp", "daniel");
+        c2.Depositar();
+        c2.PagarT();
+        c2.status();
+        c2.Sacar();
+        c2.PagarT();
+        c2.FecharC();
 
     }
 }
--- untitled/src/main/java/org/example/conta.java
+++ untitled/src/main/java/org/example/conta.java
@@ -1,13 +1,16 @@
 package org.example;
+import java.util.Scanner;
+
 
 public class conta{
-    int cont = 0;
-    int num = 0;
-    String tipo;
-    String dono;
-    double saldo;
-    boolean status;
+    public static int cont = 0;
+    public int num = 0;
+    protected String tipo;
+    private String dono;
+    private double saldo;
+    private boolean status;
 
+    Scanner sc = new Scanner(System.in);
 
 
     public conta(String t, String d) {
@@ -25,26 +28,48 @@ public class conta{
         }
     }
     public void status(){
-        System.out.println(" o numero da conta e " + this.num);
-        System.out.println("o dono " + this.dono);
-        System.out.println("tipo  " + this.tipo);
-        System.out.println("saldo " + this.saldo);
+        System.out.println("N=" + this.num);
+        System.out.println("Pertence a " + this.dono);
+        System.out.println("tipo : " + this.tipo);
+        System.out.println("saldo = " + this.saldo);
     }
 
     public void FecharC(){
-
+        if (this.saldo < 0) {
+            System.out.println("PAGUE SUAS DIVIDAS IMEDIATAMENTE!!!!!!!!!");
+        }
+        status = false;
+        System.out.println("CONTA ENCERRADA");
     }
 
     public void Sacar(){
-
+        double s;
+        System.out.println("Qual o valor do saque? ");
+        s = sc.nextDouble();
+        saldo = saldo - s;
+        System.out.println(saldo);
     }
 
-    public void Depositar(){
+    public double Depositar(){
+        double d;
+        System.out.println("Quanto q voce vai depositar?");
+        d = sc.nextDouble();
+        saldo = saldo + d;
+        System.out.println(saldo);
 
+        return d;
     }
 
     public void PagarT(){
-
+        if (tipo.equals("cc")) {
+            this.saldo = saldo - 12.00;
+        }
+        if (tipo.equals("cp")) {
+            this.saldo = saldo - 20.00;
+        }
+        if (saldo > 0) {
+            System.out.println("DIVIDAS");
+        }
     }
 
 }
